763e2f75b6d18e8c678d75634b332b6b34188b84
CristianPessatti <criss.p.a@hotmail.com>
2021-03-22 22:43:28 -0300

    adicionando exercicio 1

---
+++ criando_e_alterando_tabelas.sql
@@ -0,0 +1,23 @@
+CREATE TABLE carro (
+	 id_carro		INT(11) PRIMARY KEY AUTO_INCREMENT
+	,modelo			VARCHAR(50)
+    ,cor			VARCHAR(15)
+    ,ano			YEAR(4)
+    ,ano_modelo		YEAR(4)
+);
+
+CREATE TABLE cliente (
+	 id_cliente		INT(11) PRIMARY KEY AUTO_INCREMENT
+	,nome			VARCHAR(50)
+    ,nascimento		DATE
+);
+
+CREATE TABLE vendedor (
+	 id_vendedor 	INT(11) PRIMARY KEY AUTO_INCREMENT
+	,nome			VARCHAR(50)	
+    ,nascimento		DATE
+);
+
+ALTER TABLE carro RENAME carros;
+ALTER TABLE cliente RENAME clientes;
+ALTER TABLE vendedor RENAME vendedores;
